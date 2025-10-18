859bbdc9ffe5bda7465111e21bad8b0bbf4c123f
Aryan Tripathi <aryanapril4@gmail.com>
2025-10-18 11:41:52 +0530

    Your commit message

--- client/src/components/QuerySection.jsx
+++ client/src/components/QuerySection.jsx
@@ -23,7 +23,7 @@ const QuerySection = () => {
     setError("");
 
     try {
-      const res = await fetch("http://localhost:5000/send-email", {
+      const res = await fetch("https://medcron-healthcare.onrender.com/send-email", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
